0ab4f3f7c5a3fdc8ee0af603be60247736d0df3e
Mariasatelier <91706311+Mariasatelier@users.noreply.github.com>
2022-02-02 21:06:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,299 @@
+var itemCmp = document.querySelector('.item-content');
+var imgItem = document.querySelector('.img-item');
+var titleItem = document.querySelector('.title-item');
+var valorItem = document.querySelector('.valor-item');
+var pacelaItem = document.querySelector('.pacela-item');
+var descricaoItem = document.querySelector('.descricao-item');
+var bxX = document.querySelector('.bx-x');
+var cdp = document.querySelector('.cdp');
+var body = document.querySelector('body');
+var item1 = document.querySelector('.item-1');
+var item2 = document.querySelector('.item-2');
+var item3 = document.querySelector('.item-3');
+var item4 = document.querySelector('.item-4');
+var item5 = document.querySelector('.item-5');
+var item6 = document.querySelector('.item-6');
+var item7 = document.querySelector('.item-7');
+var item8 = document.querySelector('.item-8');
+var item9 = document.querySelector('.item-9');
+var item10 = document.querySelector('.item-10');
+var item11 = document.querySelector('.item-11');
+var item12 = document.querySelector('.item-12');
+var item13 = document.querySelector('.item-13');
+var item14 = document.querySelector('.item-14');
+var item15 = document.querySelector('.item-15');
+var item16 = document.querySelector('.item-16');
+var tamanhoLabel = document.querySelector('.tamanho-item-label');
+var tamanhoInput = document.querySelector('.tamanho-item-input');
+
+item1.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/59pbZqGS/Whats-App-Image-2022-01-26-at-16-36-33-4-removebg-preview-1.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa de girassóis com tecido de tricoline"
+  valorItem.innerHTML = "R$17,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com estampa de girassóis com tecido de tricoline. No kit contém apenas a capa e não acompanha o mdf."
+  cdp.value = "GFCF"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item2.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/DZn3N8PN/Whats-App-Image-2022-01-27-at-18-13-00-1-removebg-preview.png"
+  titleItem.innerHTML = "Babador para bebê com panda estampado no babador"
+  valorItem.innerHTML = "R$--,--"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Babador para bebê com panda estampado no babador para seu filho(a) não se sujar quando come."
+  cdp.value = "STCZ"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item3.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/N0hc1N9B/Whats-App-Image-2022-01-27-at-18-13-02-4-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa de minnie - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada super confortavel com tecido 100% algodão."
+  cdp.value = "SPFC"
+  tamanhoInput.style.display = "block"
+  tamanhoLabel.style.display = "block"
+});
+
+item4.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/286rmLNR/Whats-App-Image-2022-01-27-at-18-13-02-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa de cor rosa - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa de cor rosa super confortavel com tecido 100% algodão."
+  cdp.value = "IBIS"
+  tamanhoInput.style.display = "block"
+  tamanhoLabel.style.display = "block"
+});
+
+item5.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/rFPcpyqd/Whats-App-Image-2022-01-27-at-18-13-03-2-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa personalizada de minions - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada dos minions super confortavel com tecido 100% algodão."
+  cdp.value = "4EFD"
+  tamanhoInput.style.display = "block"
+  tamanhoLabel.style.display = "block"
+});
+
+item6.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/T1PMRFSq/Whats-App-Image-2022-01-27-at-18-13-04-5-removebg-preview.png"
+  titleItem.innerHTML = "Jogo americano duplo com estampa quadriculada branco e vermelho e do outro lado florido"
+  valorItem.innerHTML = "R$26,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Jogo americano duplo com estampa quadriculada branco e vermelho e do outro lado florido para embelezar sua mesa."
+  cdp.value = "QEF5"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item7.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/13VSVxzv/Whats-App-Image-2022-01-27-at-18-13-04-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa personalizada do homem aranha - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada do homem aranha super confortavel com tecido 100% algodão"
+  cdp.value = "UIYV"
+  tamanhoInput.style.display = "block"
+  tamanhoLabel.style.display = "block"
+});
+
+item8.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/hjsRRcCt/Whats-App-Image-2022-01-27-at-18-13-05-removebg-preview.png"
+  titleItem.innerHTML = "Jogo americano simples com estampa em mandala"
+  valorItem.innerHTML = "R$ 14,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Jogo americano simples com estampa em mandala para embelezar sua mesa."
+  cdp.value = "ABC3"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item9.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/NFPhLbvh/Whats-App-Image-2022-01-27-at-18-13-06-removebg-preview.png"
+  titleItem.innerHTML = "Porta guardanapo de laço com estampa em flores"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Porta guardanapo de laço com estampa em flores vermelhas para acompanhar o seu guardanapo."
+  cdp.value = "HBLO"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item10.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/d08Kn8rd/Whats-App-Image-2022-01-27-at-18-13-06-2-removebg-preview.png"
+  titleItem.innerHTML = "Preendedor de guardanapo em laço com estamapa natalina de natal vermelho HOHOHO"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Preendedor de guardanapo em laço com estamapa natalina de natal vermelho HOHOHO. No kit contém apenas o preendedor de guardanapo em laço."
+  cdp.value = "kYGY"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item11.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/Dy8kRV17/Whats-App-Image-2022-01-27-at-18-15-20-2-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor branca"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor branca com tecido 100% algodão"
+  cdp.value = "L0V3"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item12.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/J7TCT7DT/Whats-App-Image-2022-01-27-at-18-15-21-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor rosa claro"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor rosa claro com tecido 100% algodão"
+  cdp.value = "UYYU"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item13.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/pLHL4xB3/Whats-App-Image-2022-01-26-at-16-36-33-1-1-removebg-preview.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa natalina branca - com tecido 100% algodão"
+  valorItem.innerHTML = "R$21,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com tecido 100% algodão. No kit contém apenas uma capa de sousplat sem MDF."
+  cdp.value = "UKYU"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item14.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/5ywX5Hp2/Whats-App-Image-2022-01-26-at-16-36-33-2-removebg-preview.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa natalina vermelha - Tecido 100% algodão"
+  valorItem.innerHTML = "R$21,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com tecido 100% algodão. No kit contém apenas uma capa de sousplat sem MDF."
+  cdp.value = "JYGY"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item15.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/qMThJLMh/Whats-App-Image-2022-01-27-at-18-13-01-3-removebg-preview.png"
+  titleItem.innerHTML = "Capa para botijão com estampa natalina branca"
+  valorItem.innerHTML = "R$18,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa para botijão com estampa natalina branca para embelezar sua cozinha."
+  cdp.value = "OCIH"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
+
+item16.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/d08Kn8rd/Whats-App-Image-2022-01-27-at-18-13-06-2-removebg-preview.png"
+  titleItem.innerHTML = "Preendedor de guardanapo em laço com estamapa natalina de natal vermelho HOHOHO"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Descrição: Preendedor de guardanapo em laço com estamapa natalina de natal vermelho HOHOHO. No kit contém apenas o preendedor de guardanapo em laço."
+  cdp.value = "kYGY"
+  tamanhoInput.style.display = "none"
+  tamanhoLabel.style.display = "none"
+});
